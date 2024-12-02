3f88c7c055b7fc5dbf0f8a8ca50ca397d9531492
Ibrahim <ibrahim.borba@gmail.com>
2024-12-01 23:46:45 -0300

    feat: grid with randomness

---
+++ 2d.js
@@ -0,0 +1,51 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  const count = 20;
+
+  const createGrid = () => {
+    const points = [];
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+            radius: random.gaussian() * 0.02,
+           position: [ u, v ]
+        });
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 100;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    points.forEach((data) => {
+			const {
+				position,
+				radius
+			} = data;
+			const [u, v] = position;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, Math.abs(radius * width), 0, Math.PI * 2);
+      context.fillStyle = 'black';
+      context.fill();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
