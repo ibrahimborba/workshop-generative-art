4ff7afa47cb75136d3e4786aec5175902b46dc93
Ibrahim <ibrahim.borba@gmail.com>
2024-12-01 22:42:07 -0300

    feat: add to exercise 00

--- src/2d/00-pink-square.js
+++ src/2d/00-pink-square.js
@@ -8,6 +8,10 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'pink';
     context.fillRect(0, 0, width, height);
+    context.beginPath();
+    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false)
+    context.fillStyle = 'red';
+    context.fill();
   };
 };
 
