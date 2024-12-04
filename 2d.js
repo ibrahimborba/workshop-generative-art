49621c731f9a915141c9c05b68969af46c95f8ba
Ibrahim <ibrahim.borba@gmail.com>
2024-12-03 23:39:32 -0300

    feat: add noise and char

--- 2d.js
+++ 2d.js
@@ -10,7 +10,7 @@ const settings = {
 const sketch = () => {
 	const colorCount = random.rangeFloor(1, 6)
 	const pallete = random.shuffle(random.pick(palletes)).slice(0, colorCount);
-  const count = 30;
+  const count = 40;
 
   const createGrid = () => {
     const points = [];
@@ -18,10 +18,12 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.2;
         points.push({
-          radius: random.gaussian() * 0.02,
+          radius,
           position: [ u, v ],
-					color: random.pick(pallete)
+					color: random.pick(pallete),
+          rotation: random.noise2D(u, v),
         });
       }
     }
@@ -39,16 +41,25 @@ const sketch = () => {
 			const {
 				position,
 				radius,
-				color
+				color,
+        rotation
 			} = data;
 			const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, Math.abs(radius * width), 0, Math.PI * 2);
+      // context.beginPath();
+      // context.arc(x, y, Math.abs(radius * width), 0, Math.PI * 2);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('=', 0, 0);
+      context.restore();
     });
   };
 };
